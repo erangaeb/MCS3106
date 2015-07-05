1dad26f76dd2fc50265ddc54ed674eef60252a6b
eranga herath <erangaeb@gmail.com>
2015-07-05 17:14:53 +0530

    refactore assignment9

--- assignment/src/main/java/com/mcs/assignment/ui/Assignment9Fragment.java
+++ assignment/src/main/java/com/mcs/assignment/ui/Assignment9Fragment.java
@@ -2,29 +2,27 @@ package com.mcs.assignment.ui;
 
 import android.app.SearchManager;
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.Typeface;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v4.app.Fragment;
-import android.util.Log;
-import android.view.*;
-import android.widget.RelativeLayout;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.SearchView;
-import android.widget.Toast;
 
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.*;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.mcs.assignment.R;
+import com.mcs.assignment.utils.ActivityUtils;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -36,17 +34,15 @@ import java.util.List;
  *
  * @author eranga herath(eranga.herath@pagero.com)
  */
-public class Assignment9Fragment extends Fragment implements View.OnClickListener, SearchView.OnQueryTextListener {
+public class Assignment9Fragment extends Fragment implements SearchView.OnQueryTextListener {
 
-    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private GoogleMap googleMap;
     private MapView mapView;
 
     private SearchView searchView;
     private MenuItem searchMenuItem;
 
-    List ll;
-    // use custom font here
-    private Typeface typeface;
+    private List<LatLng> locationList;
 
     /**
      * {@inheritDoc}
@@ -61,22 +57,21 @@ public class Assignment9Fragment extends Fragment implements View.OnClickListene
         mapView.onCreate(savedInstanceState);
 
         // Gets to GoogleMap from the MapView and does initialization stuff
-        mMap = mapView.getMap();
-        mMap.getUiSettings().setMyLocationButtonEnabled(false);
-        mMap.setMyLocationEnabled(true);
+        googleMap = mapView.getMap();
+        googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+        googleMap.setMyLocationEnabled(true);
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
 
         MapsInitializer.initialize(this.getActivity());
-
-        // Updates the location and zoom of the MapView
-        //CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(43.1, -87.9), 10);
-        //mMap.animateCamera(cameraUpdate);
 
         return root;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        // TODO Add your menu entries here
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.search_menu, menu);
 
@@ -89,15 +84,12 @@ public class Assignment9Fragment extends Fragment implements View.OnClickListene
         searchView.setOnQueryTextListener(this);
     }
 
-
     /**
      * {@inheritDoc}
      */
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-
-        initUi();
     }
 
     /**
@@ -106,106 +98,78 @@ public class Assignment9Fragment extends Fragment implements View.OnClickListene
     @Override
     public void onResume() {
         super.onResume();
-        mapView.onResume();
-        setUpMap();
 
-        //setUpMapIfNeeded();
+        mapView.onResume();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void onDestroy() {
         super.onDestroy();
         mapView.onDestroy();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void onLowMemory() {
         super.onLowMemory();
         mapView.onLowMemory();
     }
 
-    private void initUi() {
-        typeface = Typeface.createFromAsset(getActivity().getAssets(), "fonts/vegur_2.otf");
-    }
-
     /**
-     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-     * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call {@link #setUpMap()} once when {@link #mMap} is not null.
-     * <p/>
-     * If it isn't installed {@link com.google.android.gms.maps.SupportMapFragment} (and
-     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
-     * install/update the Google Play services APK on their device.
-     * <p/>
-     * A user can return to this FragmentActivity after following the prompt and correctly
-     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
-     * have been completely destroyed during this process (it is likely that it would only be
-     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
-     * method in {@link #onResume()} to guarantee that it will be called.
+     * {@inheritDoc}
      */
-    private void setUpMapIfNeeded() {
-        // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getActivity().getSupportFragmentManager().findFragmentById(R.id.mapview))
-                    .getMap();
-
-            // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                setUpMap();
-            }
-        }
-    }
-
-    /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
-     * <p/>
-     * This should only be called once and when we are sure that {@link #mMap} is not null.
-     */
-    private void setUpMap() {
-        mMap.getUiSettings().setZoomControlsEnabled(true);
-    }
-
-    private void showLocation(String location) {
-        if(Geocoder.isPresent()){
-            try {
-                Geocoder gc = new Geocoder(this.getActivity());
-                List<Address> addresses= gc.getFromLocationName(location, 5); // get the found Address Objects
-
-                ll = new ArrayList(addresses.size()); // A list to save the coordinates if they are available
-                System.out.println(addresses.size() + "----------");
-                for(Address a : addresses){
-                    if(a.hasLatitude() && a.hasLongitude()){
-                        ll.add(new LatLng(a.getLatitude(), a.getLongitude()));
-                    }
-                }
-            } catch (IOException e) {
-                // handle the exception
-            }
-        }
-        LatLng currentCoordinates = (LatLng)ll.get(0);
-        mMap.addMarker(new MarkerOptions().position(currentCoordinates).title("My location").icon(BitmapDescriptorFactory.fromResource(R.drawable.bluedot)));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentCoordinates, 10));
-    }
-
-    @Override
-    public void onClick(View v) {
-    }
-
     @Override
     public boolean onQueryTextSubmit(String query) {
-        //searchView.setIconified(true);
+        // close search view
         searchView.onActionViewCollapsed();
-        System.out.println(query);
+        getActivity().supportInvalidateOptionsMenu();
+        ActivityUtils.hideSoftKeyboard(this.getActivity());
+
         showLocation(query.trim());
 
         return false;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public boolean onQueryTextChange(String newText) {
         return false;
+    }
+
+    /**
+     * Search given city and display in the map
+     *
+     * @param location city
+     */
+    private void showLocation(String location) {
+        if (Geocoder.isPresent()) {
+            try {
+                Geocoder geocoder = new Geocoder(this.getActivity());
+                List<Address> addresses = geocoder.getFromLocationName(location, 5);
+
+                // A list to save the coordinates if they are available
+                locationList = new ArrayList(addresses.size());
+                for (Address address : addresses) {
+                    if (address.hasLatitude() && address.hasLongitude()) {
+                        locationList.add(new LatLng(address.getLatitude(), address.getLongitude()));
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // display location/city
+        LatLng currentCoordinates = locationList.get(0);
+        googleMap.addMarker(new MarkerOptions().position(currentCoordinates).title(location).icon(BitmapDescriptorFactory.fromResource(R.drawable.bluedot)));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentCoordinates, 10));
     }
 
 }
